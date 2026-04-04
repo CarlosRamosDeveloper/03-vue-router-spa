e1ec7fc7fb56582f5ed013ddb5cce8f2e43afe5a
Carlos Ramos <carlosramosdev@protonmail.com>
2026-04-04 18:34:16 +0200

    added home and feature templates

--- src/modules/landing/pages/HomePage.vue
+++ src/modules/landing/pages/HomePage.vue
@@ -1,7 +1,14 @@
 <template>
-    <div>
-        <h1>Página principal</h1>
-    </div>
+    <main class="flex-1 flex items-center justify-center py-6">
+      <div class="text-center">
+        <h1 class="text-4xl font-bold tracking-tighter sm:text-5xl">
+          Bienvenido a nuestro sitio web
+        </h1>
+        <p class="mx-auto max-w-150 text-gray-500 md:text-xl">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </p>
+      </div>
+    </main>
 </template>
 
 <script setup lang="ts">
